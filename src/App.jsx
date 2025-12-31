d8c6077161e5a1a61968467ef686095ae022ca7f
Durgesh <dtarkar0425@gmail.com>
2026-01-01 01:36:45 +0530

    Updated UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-function App() {
-  return (
-    <div>Hello World</div>
-  )
-}
+import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
+import SinglePage from "./routes/singlePage/singlePage";
+import Layout from "./routes/layout/layout";
+import ProfilePage from "./routes/profilePage/profilePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-export default App
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+export default App;
+
+
